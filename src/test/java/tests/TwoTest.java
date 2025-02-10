a669b2c662d9986f78981eba13b772b585dc1d65
nikitaNGit <nikitanikam0799@gmail.com>
2025-02-10 12:56:35 +0530

    Update TwoTest.java

--- src/test/java/tests/TwoTest.java
+++ src/test/java/tests/TwoTest.java
@@ -16,7 +16,7 @@ public class TwoTest extends Base{
 		System.out.println("Nikita has updated code is");
 		System.out.println("Nikita updated back this code");
 		System.out.println("Nikita updated back this code 667777");
-		
+		System.out.println("Nikita updated back this code 555445");
 		driver = intializeDricver();
 		driver.get("https://www.google.com/");
 		System.out.println("this is test two");
